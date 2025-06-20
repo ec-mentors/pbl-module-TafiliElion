30d21835656919a99b81718d80fbeac8450701a3
Elion <tafilielion67@gmail.com>
2025-06-20 10:57:49 +0200

    Initial commit :3

---
+++ src/main/java/pbl_module/conlang_creator/ConlangCreatorApplication.java
@@ -0,0 +1,13 @@
+package pbl_module.conlang_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConlangCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConlangCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pbl_module/conlang_creator/ConlangCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pbl_module.conlang_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConlangCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
