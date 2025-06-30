53f5beff9cd24d3adb08d1f463fc30222c938fb6
Elion <tafilielion67@gmail.com>
2025-06-30 10:37:10 +0200

    Remove unnecessary line from repositories.

--- src/main/java/pbl_module/conlang_creator/repositories/MorphemeRepository.java
+++ src/main/java/pbl_module/conlang_creator/repositories/MorphemeRepository.java
@@ -3,8 +3,6 @@ package pbl_module.conlang_creator.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pbl_module.conlang_creator.model.Morpheme;
 
-import java.util.List;
 
 public interface MorphemeRepository extends JpaRepository<Morpheme, Long> {
-    List<Morpheme> findByLanguageProjectId(Long projectId);
 }
