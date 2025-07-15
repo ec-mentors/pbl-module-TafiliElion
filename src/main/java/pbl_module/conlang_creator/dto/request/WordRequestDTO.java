408d25dde619d5b85f330259bfd822db136253f2
Elion <tafilielion67@gmail.com>
2025-07-15 10:26:29 +0200

    Add frontend functionality for Words, i.e. can add words, delete words and get a list of them.

--- src/main/java/pbl_module/conlang_creator/dto/request/WordRequestDTO.java
+++ src/main/java/pbl_module/conlang_creator/dto/request/WordRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,5 +16,5 @@ public class WordRequestDTO {
     private String meaning;
     private String partOfSpeech;
     private Long languageProjectId;
-    private List<Long> morphemeIds;
+    private List<Long> morphemeIds = new ArrayList<>();
 }
