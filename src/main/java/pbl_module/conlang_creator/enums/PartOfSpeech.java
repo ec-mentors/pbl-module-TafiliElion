2ac0ec7c2ebde5a9d153ecedc9f1574a8fb0309b
Elion <tafilielion67@gmail.com>
2025-08-11 10:16:01 +0200

    Add enums for partOfSpeech, type and category fields for Word, Morpheme and Phoneme.

---
+++ src/main/java/pbl_module/conlang_creator/enums/PartOfSpeech.java
@@ -0,0 +1,14 @@
+package pbl_module.conlang_creator.enums;
+
+public enum PartOfSpeech {
+    NOUN,
+    VERB,
+    ADJECTIVE,
+    ADVERB,
+    PRONOUN,
+    PREPOSITION,
+    CONJUNCTION,
+    INTERJECTION,
+    DETERMINER,
+    PARTICLE
+}
